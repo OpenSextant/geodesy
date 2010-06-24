645c298adb3222a5e8ac44779eb74fcf61411fa2
Jason Mathews <mathews@mitre.org>
2010-06-24 19:46:21 +0000

    cleanup msg
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/gis-geodesy@85 8ce8714e-ff91-11dd-8fa0-9b777032019e

--- trunk/geodesy/test/org/mitre/itf/geodesy/test/TestGeodetic3DBounds.java
+++ trunk/geodesy/test/org/mitre/itf/geodesy/test/TestGeodetic3DBounds.java
@@ -123,7 +123,7 @@ public class TestGeodetic3DBounds extends TestCase {
         assertTrue(bbox2.contains(pt));
         bbox1.include(bbox2);
         assertTrue(bbox1.contains(bbox2));
-        assertTrue(bbox1.contains(pt)); // sometimes this test fails: see comment below
+        assertTrue("bbox contains point", bbox1.contains(pt)); // sometimes this test fails: see comment below
 
         /*
          try {
