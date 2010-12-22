bcc8cf8fe04feaa1a03adc9a6f2952d5a8955e90
Paul Silvey <psilvey@mitre.org>
2010-12-22 16:18:59 +0000

    Added ISO 8601 DateTime point and interval classes, with tests.  These classes, like others in this package, have a String arg constructor that parses standard format values, and a toString method which produces strings that the constructor will parse correctly.
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/gis-geodesy@119 8ce8714e-ff91-11dd-8fa0-9b777032019e

---
+++ trunk/geodesy/src/org/mitre/itf/geodesy/ISO8601DateTimePoint.java
@@ -0,0 +1,143 @@
+/************************************************************************************
+ * ISO8601DateTimePoint.java 12/22/10 10:16 AM psilvey
+ *
+ * (C) Copyright psilvey 2010
+ *
+ *
+ * The program is provided "as is" without any warranty express or implied, including
+ * the warranty of non-infringement and the implied warranties of merchantability and
+ * fitness for a particular purpose.  The Copyright owner will not be liable for any
+ * damages suffered by you as a result of using the Program. In no event will the
+ * Copyright owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the possibility of
+ * their occurrence.
+ *
+ ***********************************************************************************/
+package org.mitre.itf.geodesy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * The ISO8601DateTimePoint class is a simple wrapper for a time instant standardized
+ * as a number of milliseconds since midnight Jan 1, 1970 GMT. This class has a
+ * String constructor and toString method that respectively parse and format dates
+ * and times in the ISO 8601 format. Partial dates are parsed to the beginning of the
+ * time interval they define.
+ */
+public class ISO8601DateTimePoint implements Comparable<ISO8601DateTimePoint> {
+    protected static final String INVALID_POINT = "invalid time point";
+
+    protected final static TimeZone UTC_TIMEZONE = TimeZone.getTimeZone("UTC");
+    protected final static String dtSuffix = "-01-01T00:00:00.000GMT-00:00";
+    protected final static SimpleDateFormat DF;
+
+    static {
+        DF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSz");
+        DF.setTimeZone(UTC_TIMEZONE);
+    }
+
+    protected long startTime;
+
+    /**
+     * Constructor using the current local time for start time
+     */
+    public ISO8601DateTimePoint() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Constructor using the specified start time
+     *
+     * @param startTime long start time for interval
+     */
+    public ISO8601DateTimePoint(long startTime) {
+        this.startTime = startTime;
+    }
+
+    /**
+     * Constructor that takes an ISO Date Time String to be interpreted as an
+     * instant in time (start of interval if partial date time specified).
+     *
+     * @param isoDateTimeStr String containing an ISO 8601 Date Time point or interval
+     * @throws IllegalArgumentException if a parsing error occurs
+     */
+    public ISO8601DateTimePoint(String isoDateTimeStr) {
+        try {
+            String toParse = isoDateTimeStr;
+            if (toParse.endsWith("Z"))
+                toParse = toParse.substring(0, toParse.length() - 1);
+            int eoy = toParse.indexOf("-");
+            if (eoy < 0) eoy = toParse.length();
+            int n = toParse.length();
+            toParse = toParse.replace(" ", "T");
+            toParse += dtSuffix.substring(n - eoy);
+            this.startTime = DF.parse(toParse).getTime();
+            // Final validation that yyyy-MM-dd is a valid day (round trip test)
+            String ymdInput = toParse.substring(0, toParse.indexOf("T"));
+            String ymdOutput = this.toString();
+            ymdOutput = ymdOutput.substring(0, ymdOutput.indexOf("T"));
+            if (!ymdInput.equals(ymdOutput)) throw new Exception();
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("Invalid ISO 8601 date and time, " +
+                    isoDateTimeStr);
+        }
+    }
+
+    /**
+     * Getter method for this pont's start time
+     *
+     * @return long end time
+     */
+    public long getStartTime() {
+        return this.startTime;
+    }
+
+    /**
+     * Setter method for this point's start time
+     *
+     * @param startTime long start time
+     */
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ISO8601DateTimePoint that = (ISO8601DateTimePoint) o;
+        return this.startTime == that.startTime;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (this.startTime ^ (this.startTime >>> 32));
+    }
+
+    /**
+     * Returns the value 0 if this ISO8601DateTimePoint is equal to the argument
+     * ISO8601DateTimePoint; a value less than 0 if this ISO8601DateTimePoint is
+     * numerically less than the argument ISO8601DateTimePoint; and a value greater
+     * than 0 if this ISO8601DateTimePoint is numerically greater than the argument
+     * ISO8601DateTimePoint (signed comparison of long start times).
+     */
+    @Override
+    public int compareTo(ISO8601DateTimePoint that) {
+        return (this.startTime == that.startTime) ?
+                0 : (this.startTime < that.startTime) ? -1 : + 1;
+    }
+
+    /**
+     * Format this TmPoint object using the ISO 8601 point syntax, which can
+     * be parsed using the String argument constructor method of this class.
+     *
+     * @return String ISO 8601 Date Time point syntax for this TmPoint
+     */
+    @Override
+    public String toString() {
+        return DF.format(new Date(this.startTime)).replace("UTC", "Z");
+    }
+}
---
+++ trunk/geodesy/test/org/mitre/itf/geodesy/test/TestISO8601DateTime.java
@@ -0,0 +1,110 @@
+/************************************************************************************
+ * TestISO8601DateTime.java 12/22/10 11:08 AM psilvey
+ *
+ * (C) Copyright psilvey 2010
+ *
+ *
+ * The program is provided "as is" without any warranty express or implied, including
+ * the warranty of non-infringement and the implied warranties of merchantability and
+ * fitness for a particular purpose.  The Copyright owner will not be liable for any
+ * damages suffered by you as a result of using the Program. In no event will the
+ * Copyright owner be liable for any special, indirect or consequential damages or
+ * lost profits even if the Copyright owner has been advised of the possibility of
+ * their occurrence.
+ *
+ ***********************************************************************************/
+package org.mitre.itf.geodesy.test;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.mitre.itf.geodesy.ISO8601DateTimeInterval;
+import org.mitre.itf.geodesy.ISO8601DateTimePoint;
+
+import java.util.Random;
+
+public class TestISO8601DateTime extends TestCase {
+    final static Random rand = new Random();
+
+    @Test
+    public static void testTimePoint() throws Exception {
+        ISO8601DateTimePoint t1;
+        ISO8601DateTimePoint t2;
+
+        for (int trials = 0; trials < 1000; trials++) {
+            // Create a random point in time (within about 8000 years of the Epoch)
+            long rTime = (long) (100.0 * 365.25 * 24.0 *
+                    60.0 * 60.0 * 1000.0 * rand.nextDouble());
+            t1 = new ISO8601DateTimePoint(rTime);
+            String isoDateTime = t1.toString();
+            t2 = new ISO8601DateTimePoint(isoDateTime);
+            assertEquals(t1, t2);
+            assertEquals(isoDateTime, t2.toString());
+        }
+
+        String[] partials = {
+                "1970",
+                "1970-01",
+                "1970-01-01",
+                "1970-01-01T00",
+                "1970-01-01T00:00",
+                "1970-01-01T00:00:00",
+                "1970-01-01T00:00:00.000",
+                "1970-01-01T00:00:00.000Z"
+        };
+        for (String isoStr : partials) {
+            t1 = new ISO8601DateTimePoint(isoStr);
+            System.out.println(isoStr + " means " + t1.toString());
+        }
+    }
+
+    @Test
+    public static void testTimeInterval() throws Exception {
+        ISO8601DateTimeInterval t1;
+        ISO8601DateTimeInterval t2;
+
+        for (int trials = 0; trials < 1000; trials++) {
+            // Create random points in time (within about 8000 years of the Epoch)
+            long rt1 = (long) (100.0 * 365.25 * 24.0 *
+                    60.0 * 60.0 * 1000.0 * rand.nextDouble());
+            long rt2 = (long) (100.0 * 365.25 * 24.0 *
+                    60.0 * 60.0 * 1000.0 * rand.nextDouble());
+            // Make sure they are in the correct temporal order
+            if (rt1 > rt2) {
+                long temp = rt1;
+                rt1 = rt2;
+                rt2 = temp;
+            }
+
+            t1 = new ISO8601DateTimeInterval(rt1, rt2);
+            String isoDateTime = t1.toString();
+            t2 = new ISO8601DateTimeInterval(isoDateTime);
+            assertEquals(t1, t2);
+            assertEquals(isoDateTime, t2.toString());
+        }
+
+        String[] partials = {
+                "1970",
+                "1970-01",
+                "1970-01-01",
+                "1970-01-01T00",
+                "1970-01-01T00:00",
+                "1970-01-01T00:00:00",
+                "1970-01-01T00:00:00.000",
+                "1970-01-01T00:00:00.000Z"
+        };
+        for (String isoStr : partials) {
+            t1 = new ISO8601DateTimeInterval(isoStr);
+            System.out.println(isoStr + " means " + t1.toString());
+        }
+    }
+
+    /**
+     * Main method for running class tests.
+     *
+     * @param args standard command line arguments - ignored.
+     */
+    public static void main(String[] args) {
+        JUnitCore.runClasses(TestISO8601DateTime.class);
+    }
+}
