6968e9d27b49f0b6a5323a66842ba08196a7bcce
Jason Mathews <mathews@mitre.org>
2011-06-17 13:27:51 +0000

    fix typo from last update
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/gis-geodesy@159 8ce8714e-ff91-11dd-8fa0-9b777032019e

--- trunk/geodesy/src/org/mitre/itf/geodesy/Angle.java
+++ trunk/geodesy/src/org/mitre/itf/geodesy/Angle.java
@@ -4,7 +4,7 @@
  *  (C) Copyright MITRE Corporation 2007-2008
  *
  *  The program is provided "as is" without any warranty express or implied, including 
- *  the warranty of non-infringement and the implied warranties of merchantability' and 
+ *  the warranty of non-infringement and the implied warranties of merchantability and 
  *  fitness for a particular purpose.  The Copyright owner will not be liable for any 
  *  damages suffered by you as a result of using the Program.  In no event will the 
  *  Copyright owner be liable for any special, indirect or consequential damages or 
--- trunk/geodesy/src/org/mitre/itf/geodesy/NotAnMGRSBoxException.java
+++ trunk/geodesy/src/org/mitre/itf/geodesy/NotAnMGRSBoxException.java
@@ -5,7 +5,7 @@
  *
  * The program is provided "as is" without any warranty express or implied,
  * including the warranty of non-infringement and the implied warranties of
- * merchantability' and fitness for a particular purpose.  The Copyright
+ * merchantability and fitness for a particular purpose.  The Copyright
  * owner will not be liable for any damages suffered by you as a result of
  * using the Program.  In no event will the Copyright owner be liable for
  * any special, indirect or consequential damages or lost profits even if
