69f1930a1c47b7ae4d0c7e512d5f3df43e802cc2
Jason Mathews <mathews@mitre.org>
2011-06-21 12:30:02 +0000

    add testNullCompare()
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/gis-geodesy@193 8ce8714e-ff91-11dd-8fa0-9b777032019e

--- trunk/geodesy/test/org/mitre/itf/geodesy/test/TestTopocentric2DPoint.java
+++ trunk/geodesy/test/org/mitre/itf/geodesy/test/TestTopocentric2DPoint.java
@@ -38,21 +38,30 @@ public class TestTopocentric2DPoint extends TestCase {
 		assertFalse(tp.equals(tp2));
 	}
 
+    public static void testNullCompare() throws Exception {
+		double easting = 630084;
+		double northing = 4833439;
+		Topocentric2DPoint tp = new Topocentric2DPoint(easting, northing);
+		Topocentric2DPoint tp2 = null;
+		assertFalse(tp.equals(tp2));
+	}
+
 	public void testToString() {
 		double easting = 630084.12345;
 		double northing = 4833439.6789;
 		Topocentric2DPoint tp = new Topocentric2DPoint(easting, northing);
-        int prevLen = 0;
 
 		// tp.toString(0) (630084m East, 4833439m North)
 		// tp.toString(8) (630084.00000000m East, 4833439.00000000m North)
 
         String base = tp.toString();
+		assertEquals(base, tp.toString(0)); // toString() same as toString(0)
+		int prevLen = base.length();
         String prefix = base.substring(0, base.indexOf('m'));   // e.g. "100m
         // String suffix = base.substring(base.length() - 2);      // e.g. "m)"
 		// System.out.println(prefix + " " + suffix);
 
-		for(int i=0; i < 6; i++) {
+		for(int i=1; i < 6; i++) {
             String s = tp.toString(i);
             int len = s.length();
             assertTrue(len >= prevLen + 2);
